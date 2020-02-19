51f03a4a61146e7f650868d9a799174eefec0338
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-19 23:23:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,11 @@ def prime?(n)
   start = 0
   if n > 1
     range = (start..n-1).to_a
-    range.none?
+    range.none? do |n_to_test|
+      n % n_to_test == 0
+    end
+  else
+    FALSE
+  end
+end
   
