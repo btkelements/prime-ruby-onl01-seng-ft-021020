db5c79ae726f0a85b567e92197b3ae566b5f7d2c
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-19 23:25:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,10 @@ def prime?(number)
   start = 0
   if number > 1
     range = (start..number-1).to_a
+    range.none? do |n_to_test|
+      number % n_to_test == 0
+    end
+  else
+    false
+  end
+end
